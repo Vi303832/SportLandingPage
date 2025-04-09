532ff0314c5ec06077f76a11c7f66960769860ce
Akif <mehmetakiftanyerii@hotmail.com>
2025-04-10 00:11:08 +0300

    Responsive improvements

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { IoIosTennisball } from "react-icons/io";
+import { FaRunning } from "react-icons/fa";
+import { GoArrowUpRight } from "react-icons/go";
 import model from "../model.png"
 
 const Content = () => {
     return (
-        <div className="relative        pt-10">
+        <div className="relative pt-28">
             {/* Main Text */}
-            <h1 className="text-white text-[clamp(50px,10vw,150px)] flex items-center justify-center font-bold leading-none z-10 relative tracking-tight whitespace-nowrap px-4">
-                REFINE YOUR SWING
-            </h1>
+            <div className="max-w-[95%] mx-auto">
+                <h1 className="text-white text-[clamp(2rem,9vw,9rem)] flex items-center justify-center font-bold leading-none z-10 relative tracking-tighter whitespace-nowrap">
+                    LEVEL UP YOUR HEALTH
+                </h1>
 
-            <hr className="w-[95%] relative h-1 mx-auto bg-white" />
-            {/* Model Image */}
-            <div className="relative h-full w-full  ">
-                <div className="absolute top-20  w-md max-md:top-15  left-1/2 transform -translate-x-1/2 -translate-y-1/4 z-20">
-                    <img
-                        src={model}
-                        alt="Tennis Player"
-                        className="h-[600px] relative right-10 max-md:h-[400px]    object-cover"
-                    />
+                <hr className="w-full h-1 bg-white mt-2" />
+
+                {/* Model Image */}
+                <div className="relative h-full w-full">
+                    <div className="absolute top-20 w-md max-md:top-15 left-1/2 transform -translate-x-1/2 -translate-y-1/4 z-20">
+                        <img
+                            src={model}
+                            alt="Tennis Player"
+                            className="h-[600px] relative right-10 max-md:h-[400px] object-cover"
+                        />
+                    </div>
                 </div>
-            </div>
 
-
-            {/* Additional Content can be added below */}
-            <div className="mt-32 relative z-30">
-                {/* Future content will go here */}
+                {/* Additional Content */}
+                <div className="mt-32 relative z-30 flex flex-col items-start gap-4">
+                    <FaRunning className="text-3xl text-green-400" />
+                    <div className="text-white text-2xl font-semibold">Join The<br />Movement</div>
+                    <a href="#join" className="text-white text-sm flex items-center gap-1 hover:underline">
+                        OUR COMMUNITY <GoArrowUpRight className="text-lg" />
+                    </a>
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { BiLogoVenmo } from "react-icons/bi";
 import { FaCircle } from "react-icons/fa";
+
 const Navbar = () => {
     return (
-        <nav className="bg-[#272639] py-4">
-            <div className="container mx-auto px-4 flex justify-between items-center">
-                <div className="flex items-center text-white text-2xl  px-2 font-bold">
-                    <BiLogoVenmo className='text-4xl' />
+        <nav className="bg-[#272639] py-4 absolute top-0 left-0 right-0 z-50">
+            <div className="max-w-[95%] mx-auto px-4 flex justify-between items-center">
+                <div className="flex items-center text-white text-xl lg:text-2xl px-2 font-bold">
+                    <BiLogoVenmo className='text-3xl lg:text-4xl' />
                     ersus
                 </div>
 
-                <div className="hidden md:flex space-x-8">
-                    <a href="#about" className="text-white hover:text-gray-300">ABOUT</a>
-                    <a href="#how-to-use" className="text-white hover:text-gray-300">HOW TO USE</a>
-                    <a href="#features" className="text-white hover:text-gray-300">FEATURES</a>
+                <div className="hidden md:flex space-x-4 lg:space-x-8">
+                    <a href="#about" className="text-white text-sm lg:text-base hover:text-gray-300">ABOUT</a>
+                    <a href="#how-to-use" className="text-white text-sm lg:text-base hover:text-gray-300">HOW TO USE</a>
+                    <a href="#features" className="text-white text-sm lg:text-base hover:text-gray-300">FEATURES</a>
                 </div>
 
-                <button className="bg-[#E94F37] text-white px-10 py-5  flex items-center gap-2 hover:bg-[#d64632]">
-                    <FaCircle />
+                <button className="bg-[#E94F37] text-white px-4 lg:px-10 py-2 lg:py-5 text-sm lg:text-base flex items-center gap-2 hover:bg-[#d64632]">
+                    <FaCircle className="hidden lg:block" />
                     ORDER NOW
                 </button>
             </div>
